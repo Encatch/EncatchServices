50b0fed1c17ee0e0deb999a6c930fc3eea6b027d
Encatch <98411324+Encatch@users.noreply.github.com>
2025-06-08 19:14:51 +0530

    Update EncatchtechservicesApplication.java

--- Spring Security Latest ramesh/Encatchtechservices/src/main/java/com/encatchtechservices/EncatchtechservicesApplication.java
+++ Spring Security Latest ramesh/Encatchtechservices/src/main/java/com/encatchtechservices/EncatchtechservicesApplication.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-/**
- * @author Ramesh J
- * 23-Feb-2025
- */
-
 @SpringBootApplication
 public class EncatchtechservicesApplication {
 
