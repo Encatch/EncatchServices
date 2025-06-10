2ba40052734f3e5c2e2e9892297d1aa54af97623
swathi.kanduri7 <swathi.kanduri481@gmail.com>
2025-06-10 08:22:00 +0530

    pom.xml

---
+++ Spring Security Latest ramesh/Encatchtechservices/src/test/java/com/encatchtechservices/EncatchtechservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encatchtechservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncatchtechservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
